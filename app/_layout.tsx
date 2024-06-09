713a21416cf83bd142cb890a42ec1832ac45b35e
Ezeikel Pemberton <hi@ezeikel.dev>
2024-06-09 13:17:24 +0100

    chore: added tailwind and sentry. Added flash list ahead of images list ui.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,21 @@
 import { Stack } from "expo-router";
+import * as Sentry from "@sentry/react-native";
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: "https://3ced8899cf0a5a8dd3b15c539379d654:590a9050ad3be778d873c840cb48012c@o358156.ingest.us.sentry.io/4507397854330880",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+const RootLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" />
     </Stack>
   );
-}
+};
+
+export default Sentry.wrap(RootLayout);
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,22 @@
 import { Text, View } from "react-native";
+import tw from "twrnc";
+import { LinearGradient } from "expo-linear-gradient";
+import { Button } from "react-native";
 
-export default function Index() {
+const Index = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={tw`flex-1`}>
+      <LinearGradient colors={["#FFF2E6", "#FFE6CC"]} style={tw`flex-1`}>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Button
+          title="Press me"
+          onPress={() => {
+            throw new Error("Hello, again, Sentry!");
+          }}
+        />
+      </LinearGradient>
     </View>
   );
-}
+};
+
+export default Index;
