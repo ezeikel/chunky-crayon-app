c7eb26f36a1406699e1b318be9ad03e894ab9c39
Ezeikel Pemberton <hi@ezeikel.dev>
2024-06-15 10:01:27 +0100

    feat: home screen layout with column of existing coloring images working on ios and android.

--- components/ColoringImages/ColoringImages.tsx
+++ components/ColoringImages/ColoringImages.tsx
@@ -1,12 +1,28 @@
-import { Text, View, useWindowDimensions } from "react-native";
-import { SvgUri } from "react-native-svg";
+import { Text, View, Dimensions } from "react-native";
 import { Link } from "expo-router";
+import { SvgUri } from "react-native-svg";
 import { FlashList } from "@shopify/flash-list";
 import tw from "twrnc";
 import useColoringImages from "@/hooks/api/useColoringImages";
 
+// TODO: implement multi-column layout based on screen width
+// const getNumColumns = () => {
+//   if (width >= 768) {
+//     return 3; // iPad
+//   } else if (width >= 414) {
+//     return 2; // Large iPhone
+//   } else {
+//     return 1; // Small iPhone
+//   }
+// };
+
+const screenWidth = Dimensions.get("window").width;
+const padding = 20;
+const numColumns = 1;
+// const columnWidth = width / numColumns;
+const squareSize = (screenWidth - padding * 2) / numColumns;
+
 const ColoringImages = () => {
-  const { width } = useWindowDimensions();
   const { data: { coloringImages } = {}, isLoading } = useColoringImages();
 
   if (isLoading) {
@@ -17,51 +33,46 @@ const ColoringImages = () => {
     return <Text>No data</Text>;
   }
 
-  const getNumColumns = () => {
-    if (width >= 768) {
-      return 3; // iPad
-    } else if (width >= 414) {
-      return 2; // Large iPhone
-    } else {
-      return 1; // Small iPhone
-    }
-  };
-
-  const numColumns = getNumColumns();
+  const Square = ({ path, svgUri }: { path: string; svgUri: string }) => (
+    <Link
+      href={path}
+      style={tw.style(`flex-1`, {
+        marginBottom: padding,
+      })}
+    >
+      <View
+        style={tw.style(
+          `bg-white rounded-lg shadow-lg justify-center items-center`,
+          {
+            width: squareSize,
+            height: squareSize,
+          },
+        )}
+      >
+        <SvgUri
+          width={squareSize - 20}
+          height={squareSize - 20}
+          uri={svgUri}
+          viewBox="0 0 1024 1024"
+        />
+      </View>
+    </Link>
+  );
 
   return (
     <FlashList
       data={coloringImages}
       renderItem={({ item }) => (
-        <Link
-          href={`/coloring-image/${item.id}`}
-          style={{
-            flex: 1 / numColumns,
-            backgroundColor: "#FFFFFF",
-            aspectRatio: 1,
-            marginVertical: 5,
-            marginHorizontal: "auto",
-          }}
-        >
-          <View
-            style={{
-              aspectRatio: 1,
-            }}
-          >
-            <SvgUri
-              uri={item.svgUrl as string}
-              style={tw`border-2 border-black`}
-              width="100%"
-              height="100%"
-              viewBox="0 0 1024 1024"
-            />
-          </View>
-          //{" "}
-        </Link>
+        <Square
+          path={`/coloring-image/${item.id}`}
+          svgUri={item.svgUrl as string}
+        />
       )}
       keyExtractor={(item) => item.id.toString()}
-      numColumns={numColumns}
-      estimatedItemSize={10}
+      estimatedItemSize={squareSize + padding}
+      contentContainerStyle={{
+        padding,
+      }}
     />
   );
 };
